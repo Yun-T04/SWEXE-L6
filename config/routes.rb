af9cbe4b0749d1d450c8dfa2252e08d06b3a04ab
Ubuntu <ubuntu@ip-172-31-35-219.ec2.internal>
2024-10-28 07:19:35 +0000

    fghjk

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :products
+  get 'top/main'
+  resources :cartitems, only: [:new, :create, :destroy]
+  resources :carts, only: [:show]
+  
+  root 'top#main'
+end
